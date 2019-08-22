4482e3bc1e5f313ef0a78070feeb29b770188afa
Greg Jordan <gregljordan@gmail.com>
2019-08-22 17:36:29 -0400

    migrations are made

--- api/server.js
+++ api/server.js
@@ -1,8 +1,34 @@
 const express = require('express');
 
+const users = require('../users/helpers.js');
+
 const server = express();
 
 server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.status(200).json({ api: 'up' });
+  });
+  
+  server.get('/hobbits', (req, res) => {
+    users.getAll()
+      .then(hobbits => {
+        res.status(200).json(hobbits);
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      });
+  });
+  
+  server.post('/hobbits', (req, res) => {
+    users.insert(req.body)
+      .then(result => {
+        res.status(201).json(result);
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      });
+  });
 
 
 
